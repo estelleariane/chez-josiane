4357966db852f995d9873aa982a16d99bf73754e
Estelle Ariane <estellesandjong@gmail.com>
2021-11-13 09:41:43 -0500

    java changes

--- Email-validation.js
+++ Email-validation.js
@@ -1,18 +1,39 @@
-function ValidateEmail(inputText)
+function ValidateForm(inputText)
 {
+
+  let x = document.forms["form1"]["firstname"].value;
+  if (x == "") {
+    alert("Name must be filled out");
+    document.form1.firstname.focus();
+    return false;
+  }
+
+  
+  let y = document.forms["form1"]["lastname"].value;
+  if (y == "") {
+    alert("Last Name must be filled out");
+    document.form1.lastname.focus();
+    return false;
+  }
+
     var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(inputText.value.match(mailformat))
     {
-        alert("Valid email address!");
+        alert("We appreciate you contacting us. One of our associates will get back in touch with you soon!Have a great day!");
         document.form1.Email.focus();
         return true;
     }
     else
     {
-        alert("You have entered an invalid email address!");
+        alert("You have entered an invalid email address! " + inputText.value);
         document.form1.Email.focus();
         return false;
     }
+}
+
+function SubmitForm()
+{
+return false
 }
 
 function myFunction() {
